5f7517744cda912b643da26aff2d0ea51a41ac84
Gabriel Brun <lydstyl@gmail.com>
2022-02-20 20:59:25 +0100

    Can add a new task to db.

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,6 +1,5 @@
 import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from "next"
-import { MongoClient } from "mongodb"
 import { connect } from "../../lib/database"
 
 interface Haiku {
@@ -16,16 +15,13 @@ export default async function handler(
 
   if (session) {
     if (req.method === "POST") {
-      // POST /api/tasks get all tasks
-      // Process a POST request
+      // POST /api/tasks add new task in db
       try {
-        // add new task in db
+        const body = req.body
+
         const { db } = await connect()
-        const haiku = db.collection<Haiku>("haiku")
-        const result = await haiku.insertOne({
-          title: "Record of a Shriveled Datum",
-          content: "No bytes, no problem. Just insert a document, in MongoDB",
-        })
+        const haiku = db.collection<Haiku>("tasks")
+        const result = await haiku.insertOne(body)
 
         const { insertedId } = result
         res.status(201)
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,30 +1,14 @@
+import { useState } from "react"
 import type { NextPage, GetServerSideProps } from "next"
 import { useSession } from "next-auth/react"
 import { connect } from "../lib/database"
 import Layout from "../components/layout"
 import AccessDenied from "../components/access-denied"
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+import { postData } from "../utils/postData"
 
-const Page: NextPage = ({ tasks,NEXTAUTH_URL }) => {
-  tasks = JSON.parse(tasks)
+const Page: NextPage = ({ tasksFromServer, NEXTAUTH_URL }) => {
+  const [tasks, setTasks] = useState(JSON.parse(tasksFromServer))
 
   const { data: session, status } = useSession()
   const loading = status === "loading"
@@ -32,13 +16,24 @@ const Page: NextPage = ({ tasks,NEXTAUTH_URL }) => {
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== "undefined" && loading) return null
 
-  const handlClick = ()=>{
-    console.log(`gb🚀 ~ handlClick ~ handlClick`, handlClick)
+  const handlClick = async () => {
+    const body = {
+      uid: session.user.email,
+      name: "new task",
+      description: "description",
+      dueDate: "",
+      labels: [],
+      activities: [],
+      checklists: [],
+      files: [],
+    }
 
-    postData(`${NEXTAUTH_URL}/api/tasks`, { answer: 42 })
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
+    const response = await postData(`${NEXTAUTH_URL}/api/tasks`, body)
+
+    body._id = response.insertedId
+
+    // show all tasks with the new one
+    setTasks([...tasks, body])
   }
 
   // If no session exists, display access denied message
@@ -58,24 +53,20 @@ const Page: NextPage = ({ tasks,NEXTAUTH_URL }) => {
       <button onClick={handlClick}>Add a task</button>
 
       <pre>{JSON.stringify(tasks, null, 4)}</pre>
-      <p>> {tasks[0].name}</p>
     </Layout>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
-
-
   const { db } = await connect()
 
   const collection = db.collection("tasks")
 
   const documents = await collection.find({}).toArray()
-  const tasks = JSON.stringify(documents)
+  const tasksFromServer = JSON.stringify(documents)
 
   return {
-    props: { tasks,NEXTAUTH_URL:process.env.NEXTAUTH_URL },
+    props: { tasksFromServer, NEXTAUTH_URL: process.env.NEXTAUTH_URL },
   }
 }
 
